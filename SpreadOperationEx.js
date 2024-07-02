4e6d03abd8203109f5a72b1260921eb1292d5356
priyonrato.das <priyodas12@gmail.com>
2024-07-02 20:05:25 +0530

    adding duplicate elements and key in array and object spread operator

--- SpreadOperationEx.js
+++ SpreadOperationEx.js
@@ -18,6 +18,39 @@ let spreadArray = [...spreadOpArrayTwo];
 
 console.log(spreadArray);
 
+spreadOpArrayOne[2] = 902;
+
 let combinedArray = [...spreadOpArrayOne, ...spreadOpArrayTwo];
 
 console.log(combinedArray);
+
+//with duplicate key
+
+let combinedArrayTest = [...[1, 2, 3, 4], ...[4, 5, 6]];
+
+console.log(combinedArrayTest);
+
+let testObject = {
+  fkey: 10,
+  skey: 223,
+};
+
+console.log(testObject);
+
+var testObjectSpread = { ...testObject };
+
+console.log(testObjectSpread);
+
+testObjectSpread = { ...testObject, ...{ tkey: 192 } };
+
+console.log(testObjectSpread);
+
+testObjectSpread = { ...testObject, lkey: 243 };
+
+console.log(testObjectSpread);
+
+//with duplicate key
+
+testObjectSpread = { ...testObject, skey: 243 };
+
+console.log(testObjectSpread);
