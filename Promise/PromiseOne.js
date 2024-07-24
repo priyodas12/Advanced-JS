5f47eea0126ee18c1975dedc04559b7206eb1e33
priyonrato.das <priyodas12@gmail.com>
2024-07-25 00:25:55 +0530

    async await impl

--- Promise/PromiseOne.js
+++ Promise/PromiseOne.js
@@ -1,35 +1,35 @@
-const promiseOne = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		console.log('promiseOne:async task');
-		resolve('Resolving current task!');
-	}, 3000);
-});
+// const promiseOne = new Promise((resolve, reject) => {
+// 	setTimeout(() => {
+// 		console.log('promiseOne:async task');
+// 		resolve('Resolving current task!');
+// 	}, 3000);
+// });
 //then() will connect to resolve
 
-promiseOne.then((data) => {
-	console.log('Promise one consumed', data);
-});
+// promiseOne.then((data) => {
+// 	console.log('Promise one consumed', data);
+// });
 
-new Promise((resolve, reject) => {
-	setTimeout(() => {
-		console.log('promiseTwo:async task');
-		resolve('Resolving current task!');
-	}, 3000);
-}).then((data) => {
-	console.log('Promise Two consumed!', data);
-});
+// new Promise((resolve, reject) => {
+// 	setTimeout(() => {
+// 		console.log('promiseTwo:async task');
+// 		resolve('Resolving current task!');
+// 	}, 3000);
+// }).then((data) => {
+// 	console.log('Promise Two consumed!', data);
+// });
 
-new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve('{name:"test"}');
-	}, 3000);
-}).then((data) => {
-	console.log('Promise Three consumed!', data);
-});
+// new Promise((resolve, reject) => {
+// 	setTimeout(() => {
+// 		resolve('{name:"test"}');
+// 	}, 3000);
+// }).then((data) => {
+// 	console.log('Promise Three consumed!', data);
+// });
 
-new Promise((resolve, reject) => {
+const promiseFour = new Promise((resolve, reject) => {
 	setTimeout(() => {
-		fetch('https://jsonplaceholder.org/users/20')
+		fetch('https://jsonplaceholder1.org/users/20')
 			.then((response) => {
 				if (!response.ok) {
 					throw new Error('Network response was not ok');
@@ -47,13 +47,25 @@ new Promise((resolve, reject) => {
 				reject(error);
 			});
 	}, 6000);
-})
-	.then((data) => {
-		console.log('Users::', data);
-	})
-	.catch((error) => {
-		console.log('Error:', error);
-	})
-	.finally((data) => {
-		console.log('promise is resolved or rejected', data);
-	});
+});
+// 	.then((data) => {
+// 		console.log('Users::', data);
+// 	})
+// 	.catch((error) => {
+// 		console.log('Error:', error);
+// 	})
+// 	.finally((data) => {
+// 		console.log('promise is resolved or rejected', data);
+// 	});
+
+async function consumePromise() {
+	console.log('consuming promise four');
+	try {
+		const response = await promiseFour;
+		console.log(response);
+	} catch (error) {
+		console.log('error', error);
+	}
+}
+
+consumePromise();
