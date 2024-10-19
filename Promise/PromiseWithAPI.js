1afb0263351d3f6403b08abd55b5480ab8978e67
Priyobrato.Das <priyodas12@gmail.com>
2024-10-20 00:25:59 +0530

    with delay

--- Promise/PromiseWithAPI.js
+++ Promise/PromiseWithAPI.js
@@ -1,22 +1,32 @@
 const apiResponse = new Promise((resolve, reject) => {
-	fetch('https://jsonplaceholder.typicode.com/posts/9')
-		.then((response) => {
-			if (!response.ok) {
-				reject(
-					'Failed to fetch data from API: Status::' + response.status,
-				);
-			} else {
-				console.log('API Response: \n', response);
-				return response.json();
-			}
-		})
-		.then((response) => {
-			console.log('Resolving API Response: \n', response);
-			resolve(response);
-		})
-		.catch((error) =>
-			reject('Failed to Extract API response, Error::' + error),
-		);
+	setTimeout(
+		() => {
+			fetch('https://jsonplaceholder.typicode.com/posts/9')
+				.then((response) => {
+					if (!response.ok) {
+						// Reject if response status is not OK
+						reject(
+							'Failed to fetch data from API: Status:: ' +
+								response.status,
+						);
+					} else {
+						// Proceed to extract JSON data if the response is OK
+						return response.json();
+					}
+				})
+				.then((data) => {
+					// This second .then will only be called if the first was successful
+					console.log('Resolving API Response: \n', data);
+					resolve(data);
+				})
+				.catch((error) => {
+					// Catch any errors from fetch or response processing
+					reject('Failed to extract API response, Error:: ' + error);
+				});
+		},
+		//setting up delay
+		2000,
+	); 
 });
 
 apiResponse
